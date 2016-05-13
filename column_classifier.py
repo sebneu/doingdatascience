eae1a19efbfd20bef6ae105eca25cc5cf27d1e17
neumaier <sebastian.neumaier@wu.ac.at>
2016-05-13 13:23:05 +0200

    initial commit

---
+++ column_classifier.py
@@ -0,0 +1,94 @@
+import json
+
+import anycsv
+import os
+from collections import defaultdict
+
+
+class ColumnLabel:
+    NUMERIC = 'NUMERIC'
+    ENTITY = 'ENTITY'
+    TEXT = 'TEXT'
+    ID = 'ID'
+
+def isfloat(value):
+  try:
+    float(value)
+    return True
+  except:
+    return False
+
+def get_label(v):
+    if v.isnumeric() or isfloat(v):
+        return ColumnLabel.NUMERIC
+    if isfloat(v):
+        return ColumnLabel.NUMERIC
+    return ColumnLabel.TEXT
+
+
+class Column:
+    def __init__(self, values):
+        self.values = values
+        self._classify()
+
+    def _classify(self):
+        self.char_dist = defaultdict(int)
+        labels = defaultdict(int)
+        lengths = defaultdict(int)
+        tokens = defaultdict(int)
+
+        for value in self.values:
+            for c in value:
+                self.char_dist[c] += 1
+
+            labels[get_label(value)] += 1
+            lengths[len(value)] += 1
+            tok = len(value.split(' '))
+            self.length = '4+' if tok >= 4 else str(tok)
+            tokens[self.length] += 1
+
+        if len(labels) == 1 and ColumnLabel.NUMERIC in labels:
+            self.label = ColumnLabel.NUMERIC
+        elif len(lengths) == 1:
+            self.label = ColumnLabel.ID
+        elif '4+' in tokens and tokens['4+'] >= len(self.values) * 0.5:
+            self.label = ColumnLabel.TEXT
+        else:
+            self.label = ColumnLabel.ENTITY
+
+
+if __name__ == '__main__':
+    col_stats = {'labels': defaultdict(int), 'lengths': defaultdict(int), 'columns': 0, 'tables': 0, 'errors': 0}
+
+    for root, subdirs, files in os.walk('tables'):
+        for filename in files:
+            if filename.endswith('.csv'):
+                try:
+
+                    path = os.path.join(root, filename)
+                    csvr = anycsv.reader(filename=path)
+                    # skip first 3 lines to avoid description and header lines
+                    h = csvr.next()
+                    h = csvr.next()
+                    h = csvr.next()
+                    while len(h) <= 1:
+                        # possibly description line
+                        h = csvr.next()
+                    # setup columns
+                    columns = [[] for _ in range(len(h))]
+                    for row in csvr:
+                        for i, cell in enumerate(row):
+                            columns[i].append(cell)
+                    for c in columns:
+                        col = Column(c)
+                        col_stats['labels'][col.label] += 1
+                        col_stats['lengths'][col.length] += 1
+                        col_stats['columns'] += 1
+                    col_stats['tables'] += 1
+                    print 'processed table: ', filename
+                except Exception as e:
+                    print e
+                    col_stats['errors'] += 1
+
+    with open('col_stats.json', 'w') as f:
+        json.dump(col_stats, f)
